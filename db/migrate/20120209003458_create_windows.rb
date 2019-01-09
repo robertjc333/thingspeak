eda1e6c9b972c88654d53d6c67cbe5a91db75f4e
robertjc333 <40555149+robertjc333@users.noreply.github.com>
2019-01-09 12:15:54 +0800

    Update 20120209003458_create_windows.rb

--- db/migrate/20120209003458_create_windows.rb
+++ db/migrate/20120209003458_create_windows.rb
@@ -4,7 +4,8 @@ class CreateWindows < ActiveRecord::Migration
       t.integer :channel_id
       t.integer :plugin_id
       t.integer :position
-
+      t.integer :id
+        
       t.timestamps
     end
   end
