f0fe72c993f1beb9df08920aa9eea67157d84db8
robertjc333 <40555149+robertjc333@users.noreply.github.com>
2019-01-09 15:48:36 +0800

    Create 20141016020850_update_public_flag_to_plugins.rb

---
+++ db/migrate/20141016020850_update_public_flag_to_plugins.rb
@@ -0,0 +1,9 @@
+class UpdatePublicFlagToPlugins < ActiveRecord::Migration
+  def change
+    #add_column :plugins, :public_flag, :boolean, default: false
+    
+    # set existing public_flag values
+    execute("UPDATE plugins SET public_flag = true WHERE private_flag = false;")
+            #UPDATE COMPANY SET ADDRESS = 'Texas' WHERE ID = 6;
+  end
+end
