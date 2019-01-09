ec46620eacba11f4a8c68c2771352c343292e6d0
robertjc333 <40555149+robertjc333@users.noreply.github.com>
2019-01-09 10:45:18 +0800

    Update 20101117211209_add_index_to_feeds.rb

--- db/migrate/20101117211209_add_index_to_feeds.rb
+++ db/migrate/20101117211209_add_index_to_feeds.rb
@@ -2,13 +2,13 @@ class AddIndexToFeeds < ActiveRecord::Migration
   def self.up
 		add_index :feeds, :device_id
                 add_index :feeds, :channel_id
-		add_index :feeds, :created_at
+		#add_index :feeds, :created_at
 	  #adddd 
   end
 
   def self.down
 		remove_index :feeds, :device_id
                 remove_index :feeds, :channel_id
-		remove_index :feeds, :created_at
+		#remove_index :feeds, :created_at
   end
 end
