92eb239bf014c0e580039209b5b09548a2c05442
robert <robert@robert-ubuntu-16.04>
2019-01-09 16:58:56 +0800

    thingspeak ubuntu 2019.01.09-new

--- db/migrate/20101117211209_add_index_to_feeds.rb
+++ db/migrate/20101117211209_add_index_to_feeds.rb
@@ -1,9 +1,13 @@
 class AddIndexToFeeds < ActiveRecord::Migration
   def self.up
 		add_index :feeds, :device_id
+                add_index :feeds, :channel_id
+		add_index :feeds, :created_at
   end
 
   def self.down
 		remove_index :feeds, :device_id
+                remove_index :feeds, :channel_id
+		remove_index :feeds, :created_at
   end
 end
