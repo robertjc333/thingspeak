d7f0c037c006735d18e631de58bf01f571cb816d
robertjc333 <40555149+robertjc333@users.noreply.github.com>
2019-01-10 10:18:00 +0800

    Update 20140717011204_move_data_from_window_details_to_windows.rb

--- db/migrate/20140717011204_move_data_from_window_details_to_windows.rb
+++ db/migrate/20140717011204_move_data_from_window_details_to_windows.rb
@@ -1,12 +1,12 @@
 class MoveDataFromWindowDetailsToWindows < ActiveRecord::Migration
   def change
-    #execute "UPDATE windows w, plugin_window_details p SET w.content_id = p.plugin_id WHERE w.id = p.plugin_window_id;"
-    #execute "UPDATE windows w, chart_window_details c SET w.content_id = c.field_number, w.options = c.options WHERE w.id = c.chart_window_id;"
+    execute "UPDATE windows w, plugin_window_details p SET w.content_id = p.plugin_id WHERE w.id = p.plugin_window_id;"
+    execute "UPDATE windows w, chart_window_details c SET w.content_id = c.field_number, w.options = c.options WHERE w.id = c.chart_window_id;"
     
-    execute "UPDATE windows SET content_id =(SELECT field_number FROM chart_window_details  WHERE chart_window_details.chart_window_id = windows.id), options = (SELECT options FROM chart_window_details  WHERE chart_window_details.chart_window_id = windows.id);"
-    #WHERE windows.id =(SELECT chart_window_id FROM chart_window_details WHERE chart_window_details.chart_window_id = windows.id );"
-    execute "UPDATE windows SET content_id =(SELECT plugin_id FROM plugin_window_details  WHERE plugin_window_details.plugin_window_id = windows.id);"
-    #WHERE windows.id =(SELECT plugin_window_id FROM plugin_window_details WHERE plugin_window_details.plugin_window_id = windows.id );"
+    #execute "UPDATE windows SET content_id =(SELECT field_number FROM chart_window_details  WHERE chart_window_details.chart_window_id = windows.id), options = (SELECT options FROM chart_window_details  WHERE chart_window_details.chart_window_id = windows.id);"
+    ##WHERE windows.id =(SELECT chart_window_id FROM chart_window_details WHERE chart_window_details.chart_window_id = windows.id );"
+    #execute "UPDATE windows SET content_id =(SELECT plugin_id FROM plugin_window_details  WHERE plugin_window_details.plugin_window_id = windows.id);"
+    ##WHERE windows.id =(SELECT plugin_window_id FROM plugin_window_details WHERE plugin_window_details.plugin_window_id = windows.id );"
   end
 end
 
